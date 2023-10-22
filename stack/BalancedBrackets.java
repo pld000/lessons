b7dc71870103ef54003cbf256a9e502d5dcce544
pld000 <pld000@yandex.ru>
2023-10-22 23:13:29 +0300

    - added stack tests

--- stack/BalancedBrackets.java
+++ stack/BalancedBrackets.java
@@ -1,16 +1,29 @@
 package stack;
 
 public class BalancedBrackets {
-    public static void main(String[] args) {
+    public static boolean checkBalance(String brackets) {
+        int controlSum = 0;
+        char closeBracket = ')';
+        char openBracket = '(';
+        Stack<Character> stack = new Stack<Character>();
 
-    }
-
-    public boolean checkBalance(String brackets) {
         for (int i = 0; i < brackets.length(); i++) {
-
+            stack.push(brackets.charAt(i));
         }
 
-        return false;
+        while (stack.size() > 0) {
+            Character bracket = stack.pop();
+
+            if (controlSum == 0 && bracket != closeBracket) {
+                return false;
+            } else if (bracket == closeBracket) {
+                controlSum++;
+            } else if (bracket == openBracket) {
+                controlSum--;
+            }
+        }
+
+        return controlSum == 0;
     }
 
 }
