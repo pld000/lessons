eb1df8170d9d3e63c939e8a859beb32c93a92309
pld000 <pld000@yandex.ru>
2023-10-23 15:26:45 +0300

    - released stack expressions

--- stack/PostfixExpressionNotation.java
+++ stack/PostfixExpressionNotation.java
@@ -1,8 +1,12 @@
 package stack;
 
+import java.util.Arrays;
+
 public class PostfixExpressionNotation {
     public Stack<Character> expressionStack;
     public Stack<Integer> resultStack;
+    private final Character[] _operations = {'+', '-', '*'};
+    private final int _delimiter = 32;
 
     public PostfixExpressionNotation() {
         this.expressionStack = new Stack<Character>();
@@ -10,8 +14,40 @@ public class PostfixExpressionNotation {
     }
 
     public void pushExpression(String expression) {
-        for (int i = expression.length() - 1; i <= 0; i--) {
-            this.expressionStack.push(expression.charAt(i));
+        for (int i = expression.length() - 1; i >= 0; i--) {
+            char value = expression.charAt(i);
+
+            if ((int) value != (int) ' ') {
+                this.expressionStack.push(expression.charAt(i));
+            }
         }
+    }
+
+    public void executeExpression() {
+        while (this.expressionStack.size() > 0) {
+            char value = this.expressionStack.pop();
+
+            if (Arrays.asList(this._operations).contains(value)) {
+                int operand1 = this.resultStack.pop();
+                int operand2 = this.resultStack.pop();
+
+                this.resultStack.push(this._execute(operand1, operand2, value));
+            } else {
+                this.resultStack.push(Character.getNumericValue(value));
+            }
+        }
+    }
+
+    private int _execute(int operand1, int operand2, char operation) {
+        switch (operation) {
+            case (int) '+':
+                return operand1 + operand2;
+            case (int) '-':
+                return operand1 - operand2;
+            case (int) '*':
+                return operand1 * operand2;
+        }
+
+        return 0;
     }
 }
---
+++ stack/PostfixExpressionNotationTest.java
@@ -0,0 +1,39 @@
+package stack;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostfixExpressionNotationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void pushExpression() {
+        PostfixExpressionNotation postfixNotation = new PostfixExpressionNotation();
+        String expression = "8 2 + 5 * 9 +";
+        postfixNotation.pushExpression(expression);
+        int lengthWithoutSpace = 7;
+
+        Assertions.assertEquals(lengthWithoutSpace, postfixNotation.expressionStack.size(), "Failed pushExpression, wrong size");
+    }
+
+    @Test
+    void executeExpression() {
+        PostfixExpressionNotation postfixNotation = new PostfixExpressionNotation();
+        String expression = "8 2 + 5 * 9 +";
+        postfixNotation.pushExpression(expression);
+        postfixNotation.executeExpression();
+
+        Assertions.assertEquals(59, postfixNotation.resultStack.peek(), "Failed executeExpression, wrong result");
+    }
+}
