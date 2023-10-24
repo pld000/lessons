721ed9c57fe81e9d5f26ff8bb19827f1a6ebbf91
pld000 <pld000@yandex.ru>
2023-10-24 23:00:29 +0300

    - added structure for queue taks

---
+++ queue/Queue.java
@@ -0,0 +1,28 @@
+package queue;
+
+import java.util.*;
+
+public class Queue<T>
+{
+    public Queue()
+    {
+        // инициализация внутреннего хранилища очереди
+    }
+
+    public void enqueue(T item)
+    {
+        // вставка в хвост
+    }
+
+    public T dequeue()
+    {
+        // выдача из головы
+        return null; // null если очередь пустая
+    }
+
+    public int size()
+    {
+        return 0; // размер очереди
+    }
+
+}
