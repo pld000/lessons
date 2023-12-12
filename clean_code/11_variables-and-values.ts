0924e2bc2c4dca76e489bf67a3573279bc0f6e63
pld000 <pld000@yandex.ru>
2023-12-12 21:53:29 +0300

    - variables and values

--- clean_code/11_variables-and-values.ts
+++ clean_code/11_variables-and-values.ts
@@ -1,4 +1,7 @@
 ////////////////////////////////// 1 //////////////////////////////////
+import { OfferOrderModel } from '../../../../../Projects/web.bakery-admin/src/app/modules/offers/models/offer-order.model';
+import { IOrderLine } from '../../../../../Projects/web.bakery-admin/src/app/modules/orders';
+
 /**
  *  Сделал объявление переменной явным, поменял модификатор let на const,
  * */
@@ -35,21 +38,26 @@ async function initUpsellProducts() {
 
 ////////////////////////////////// 4 //////////////////////////////////
 /**
- *
+ * Сделал явным объявление переменных (user, company, invoiceInfo, patch),
+ * перенес объявление переменной patch, к месту ее использования
  * */
-  private async _init() {
-    const { companyId, userId } = this._route.snapshot.params;
-    this.company = await this._companyApi.getCompany(companyId).toPromise();
-    this.user = await this._usersCustomersApi.getUserById(userId).toPromise();
-    // const invoiceInfo: IUserInvoiceInfo = await this._usersCustomersApi.getUserInvoiceInfo(userId).toPromise();
-    // const deliveryAddress = await this._usersCustomersApi.getDeliveryAddress(userId).toPromise();
-    //
-    // const patch = this._getUserFormPatch({ user: this.user, invoiceInfo, deliveryAddress});
+interface UserFormPatch {
+  user: User,
+  invoiceInfo: UserCompany,
+  company: UserInvoiceInfo
+}
 
-    this.userForm = this._getUserForm(this.bakeryId);
+async function initUserPage() {
+  const { companyId, userId } = this._route.snapshot.params;
+  const user: User = await this._usersCustomersApi.getUserById(userId);
+  const company: UserCompany = await this._companyApi.getCompany(companyId);
+  const invoiceInfo: UserInvoiceInfo = await this._usersCustomersApi.getUserInvoiceInfo(userId);
 
-    await this._loadUserOrders();
-  }
+  this.userForm = this._getUserForm(this.bakeryId);
+  const patch: UserFormPatch = this._getUserFormPatch({ user, invoiceInfo, company });
+  this.userForm.patch(patch);
+  // ...
+}
 
 ////////////////////////////////// 5 //////////////////////////////////
 /**
@@ -73,9 +81,20 @@ function isDeliveryFinished(cart: Cart): boolean {
 
 ////////////////////////////////// 6 //////////////////////////////////
 /**
- *
+ * Добавил затирание неиспользуемого значения
  * */
+function ngOnInit() {
+  const { bakeryId } = this._route.snapshot.params;
+  this.bakery = this._bakeryApi.getBakeryById(bakeryId);
+  // ...
+}
 
+// ...
+function ngOnDestroy() {
+  // ...
+  this.bakery = null;
+  // ...
+}
 
 ////////////////////////////////// 7 //////////////////////////////////
 /**
@@ -92,49 +111,152 @@ export class RequestOrderDetails {
 
 ////////////////////////////////// 8 //////////////////////////////////
 /**
- *
+ * Сделал объявления переменных явным, сделаш инициализацию счетчика цикла while,
+ * непосредственно перед циклом, переименовал переменную i в цикле while на daysBeforeOrder
  * */
+function getOrderOnDay(day: ScheduleDay, workingDaysCount: number): string {
+  let dayIndex: number = this.parsedSchedule.findIndex(d => d.key === day.key);
+  const deadlineDaysCount: number = day.deadline.value % workingDaysCount;
+  const steps: number = deadlineDaysCount === 0 ? workingDaysCount : deadlineDaysCount;
 
+  let daysBeforeOrder: number = 0;
+  while (daysBeforeOrder !== steps) {
+    const prevIndex = dayIndex - 1;
+    dayIndex = prevIndex >= 0 ? prevIndex : prevIndex + 7;
+    daysBeforeOrder = this.parsedSchedule[dayIndex].dayOff ? daysBeforeOrder : daysBeforeOrder + 1;
+  }
+
+  return this.parsedSchedule[dayIndex].key;
+}
 
 ////////////////////////////////// 9 //////////////////////////////////
 /**
- *
+ * Сделал инициализацию полей класса, улучшил типизацию заменив тип string
+ * для все полей на типы соответствующего enum
  * */
+export enum PAYMENT_STATUSES {
+}
 
+export enum PAYMENT_TYPES {
+}
+
+export enum PAYMENT_METHODS {
+}
+
+export class Order {
+  public paymentStatus: PAYMENT_STATUSES = null;
+  public paymentType: PAYMENT_TYPES = null;
+  public paymentMethod: PAYMENT_METHODS = null;
+  // ...
+}
 
 ////////////////////////////////// 10 //////////////////////////////////
 /**
- *
+ * Инициализировал аккомулирующую переменную price начальным значением
  * */
+const getCustomOrderLineTotal = (orderLine: ICustomOrderLine): number => {
+  const { customAddOns } = orderLine;
+  let price: number = 0;
 
+  customAddOns.forEach((addOn) => {
+    price += addOn.price ? addOn.price : 0;
+  });
+  price += orderLine.price ? orderLine.price * 1 : 0;
+
+  return price * orderLine.quantity;
+};
 
 ////////////////////////////////// 11 //////////////////////////////////
 /**
- *
+ * Заменил имя paramsAr на более наглядное имя queryParamsAr, добавил начальную инициализацию
+ * queryParamsAr
  * */
+const generateUrl = (snakedParams: { [key: string]: string }, apiUrl: string): string => {
+  const queryParamsAr: string[] = [];
 
+  Object.entries(snakedParams).forEach(([key, value]) => {
+    if (value && Array.isArray(value)) {
+      value.forEach(v => queryParamsAr.push(`${key}=${v}`));
+    } else if (value) {
+      queryParamsAr.push(`${key}=${value}`);
+    }
+  });
+
+  return apiUrl + '?' + queryParamsAr.join('&');
+};
 
 ////////////////////////////////// 12 //////////////////////////////////
 /**
- *
+ * Добавил сообщение о недопустимом значении переменной, учлучших консткукцию
+ * switch заменив строковые значения на значения из enum, улучших типизацию
+ * входного параметра type
  * */
+enum SANITIZER_TYPES {
+  HTML = 'html',
+  STYLE = 'style',
+  SCRIPT = 'script',
+  URL = 'url',
+  RESOURCE_URL = 'resourceUrl',
+}
 
+function transform(value: any, type: SANITIZER_TYPES): SafeHtml | SafeStyle | SafeScript | SafeUrl | SafeResourceUrl {
+  switch (type) {
+    case SANITIZER_TYPES.HTML:
+      return this.sanitizer.bypassSecurityTrustHtml(value);
+    case SANITIZER_TYPES.STYLE:
+      return this.sanitizer.bypassSecurityTrustStyle(value);
+    case SANITIZER_TYPES.SCRIPT:
+      return this.sanitizer.bypassSecurityTrustScript(value);
+    case SANITIZER_TYPES.URL:
+      return this.sanitizer.bypassSecurityTrustUrl(value);
+    case SANITIZER_TYPES.RESOURCE_URL:
+      return this.sanitizer.bypassSecurityTrustResourceUrl(value);
+    default:
+      throw new Error(`Invalid safe type specified: ${type}`);
+  }
+}
 
 ////////////////////////////////// 13 //////////////////////////////////
 /**
- *
+ * Сделал объявление переменных originExtraProductsMap, backUpExtraProductsMap явным,
+ * значения этих переменых не перезаписываются, поэтому заменил модификатор let на
+ * модификатор const, для лучшего понимания кода
  * */
-
+const mergeMappedLines = (mappedLine: CakeBuilderOrderLine, backupMappedLine: CakeBuilderOrderLine) => {
+  // ...
+  const originExtraProductsMap: { [key: number]: ExtraProduct } = mappedLine.extraProductsMap;
+  const backUpExtraProductsMap: { [key: number]: ExtraProduct } = backupMappedLine.extraProductsMap;
+  // ...
+};
 
 ////////////////////////////////// 14 //////////////////////////////////
 /**
- *
+ * Добавил начальную инициализацию полей в классе
  * */
-
+export class InnerOffer {
+  public isEdit: boolean = false;
+  public scrollTarget: { type: string, id: number } = null;
+  public isNew: boolean = false;
+  public orderOffer: OfferOrderModel = null;
+  public customOrderLine: CustomOrderLine = null;
+  public orderLine: OrderLine = null;
+  // ...
+}
 
 ////////////////////////////////// 15 //////////////////////////////////
 /**
- *
+ * Добавил корректное завершение работы с переменной textArea
  * */
+export function copyToClipboard(textToCopy: string): Promise<boolean> {
+  try {
+    let textArea = document.createElement('textarea');
 
+    // ...
+
+    textArea = null;
+    return Promise.resolve(true);
+  } catch (err) {
+    return Promise.resolve(false);
+  }
+}
 
