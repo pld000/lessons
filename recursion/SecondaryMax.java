865d1263c5ff39364f6495da27f61ecbbda506b3
pld000 <pld000@yandex.ru>
2024-01-06 21:57:22 +0300

    - refactoring

--- recursion/SecondaryMax.java
+++ recursion/SecondaryMax.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 
 public class SecondaryMax {
     public static int getSecondMax(ArrayList<Integer> numbers, int currentIndex) {
-        if (currentIndex == numbers.size() - 1) {
-            return numbers.get(numbers.size() - 2);
+        if (currentIndex == 0) {
+            return numbers.get(1);
         }
 
-        int nextIndex = currentIndex + 1;
-        int nextNextIndex = currentIndex + (currentIndex == numbers.size() - 2 ? 1 : 2);
+        int prevIndex = Math.max(currentIndex - 1, 0);
+        int prevPrevIndex = Math.max(currentIndex - 2, 0);
 
-        int min = Math.min(numbers.get(currentIndex), Math.min(numbers.get(nextIndex), numbers.get(nextNextIndex)));
-        int replacingIndex = numbers.get(currentIndex) == min ? currentIndex : numbers.get(nextIndex) == min ? nextIndex : nextNextIndex;
+        int min = Math.min(numbers.get(currentIndex), Math.min(numbers.get(prevIndex), numbers.get(prevPrevIndex)));
+        int replacingIndex = numbers.get(currentIndex) == min ? currentIndex : numbers.get(prevIndex) == min ? prevIndex : prevPrevIndex;
 
         numbers.set(replacingIndex, numbers.get(currentIndex));
         numbers.set(currentIndex, min);
 
-        return getSecondMax(numbers, ++currentIndex);
+        return getSecondMax(numbers, --currentIndex);
     }
 
     public static int getSecondMax(ArrayList<Integer> numbers) {
-        return getSecondMax(numbers, 0);
+        return getSecondMax(numbers, numbers.size() - 1);
     }
 }
