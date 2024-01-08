38247ee780015a9de28b143fdd8fda4c5250b877
pld000 <pld000@yandex.ru>
2024-01-08 21:20:04 +0300

    - refactoring

--- recursion/SecondaryMax.java
+++ recursion/SecondaryMax.java
@@ -3,16 +3,20 @@ package recursion;
 import java.util.ArrayList;
 
 public class SecondaryMax {
-    public static Integer getSecondMax(ArrayList<Integer> numbers, int currentIndex, int maxNum, int secondMaxNum) {
-        if (currentIndex == 0) {
+    public static Integer getSecondMax(ArrayList<Integer> numbers, int currentIndex, Integer maxNum, Integer secondMaxNum) {
+        if (currentIndex == 1) {
             return secondMaxNum;
         }
 
-        if (maxNum < numbers.get(currentIndex)) {
-            secondMaxNum = maxNum;
-            maxNum = numbers.get(currentIndex);
+        maxNum = maxNum == null ? Math.max(numbers.get(currentIndex), numbers.get(currentIndex - 1)) : maxNum;
+        secondMaxNum = secondMaxNum == null ? Math.min(numbers.get(currentIndex), numbers.get(currentIndex - 1)) : secondMaxNum;
+        int testingNum = numbers.get(currentIndex - 2);
+
+        if (maxNum >= testingNum) {
+            secondMaxNum = Math.max(secondMaxNum, testingNum);
         } else {
-            secondMaxNum = Math.max(secondMaxNum, numbers.get(currentIndex));
+            secondMaxNum = maxNum;
+            maxNum = testingNum;
         }
 
         return getSecondMax(numbers, --currentIndex, maxNum, secondMaxNum);
@@ -23,12 +27,10 @@ public class SecondaryMax {
             return null;
         } else if (numbers.size() == 1) {
             return numbers.get(0);
+        } else if (numbers.size() == 2) {
+            return Math.min(numbers.get(0), numbers.get(1));
         }
 
-        int currentIndex = numbers.size() - 1;
-        int maxNum = Math.max(numbers.get(currentIndex), numbers.get(currentIndex - 1));
-        int secondMaxNum = Math.min(numbers.get(currentIndex), numbers.get(currentIndex - 1));
-
-        return getSecondMax(numbers, currentIndex - 1, maxNum, secondMaxNum);
+        return getSecondMax(numbers, numbers.size() - 1, null, null);
     }
 }
