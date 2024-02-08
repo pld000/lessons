726665d8a91c080a916b7fa3a7a6dd285d596fa5
pld000 <pld000@yandex.ru>
2024-02-08 22:51:42 +0300

    - balanced bst array

--- balanced_bst_2/BalancedBSTTest.java
+++ balanced_bst_2/BalancedBSTTest.java
@@ -3,6 +3,8 @@ package balanced_bst_2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BalancedBSTTest {
@@ -15,8 +17,9 @@ class BalancedBSTTest {
 
     @Test
     void generateTree() {
-        // int[] test2 = {140, 20, 30, 150, 50, 60, 70, 90, 80, 100, 110, 120, 130, 500};
-        int[] test2 = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150};
+//        int[] test2 = {140, 20, 30, 150, 50, 60, 70, 90, 80, 100, 110, 120, 130, 500};
+        int[] test2 = {140, 20, 30, 150, 50, 60, 70, 90, 80, 100, 110, 120, 130, 500};
+//        int[] test2 = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150};
         bst.GenerateTree(test2);
 
         printBst(bst);
@@ -27,32 +30,30 @@ class BalancedBSTTest {
     }
 
     public void printBst(BalancedBST bst) {
-        _printBst(bst.Root);
+        ArrayList<ArrayList<BSTNode>> printArr = new ArrayList<>();
+        _printBst(bst.Root, printArr);
+
+        for (int i = 0; i < printArr.size(); i++) {
+            for (int j = 0; j < printArr.get(i).size(); j++) {
+                System.out.print(printArr.get(i).get(j).NodeKey + " ");
+            }
+            System.out.println();
+            System.out.println("---------------------------");
+        }
     }
 
-    private void _printBst(BSTNode node) {
+    private void _printBst(BSTNode node, ArrayList<ArrayList<BSTNode>> printArr) {
         if (node == null) {
-            //  System.out.print("null ");
             return;
         }
 
-        if (node.Parent == null) {
-            System.out.println(node.NodeKey);
+        if (printArr.size() == node.Level) {
+            printArr.add(node.Level, new ArrayList<>());
         }
 
-        if (node.LeftChild != null) {
-            System.out.print(node.LeftChild.NodeKey + " ");
-        }
+        printArr.get(node.Level).add(node);
 
-        if (node.RightChild != null) {
-            System.out.print(node.RightChild.NodeKey + " ");
-        }
-
-//        System.out.println(node.NodeKey + " --- " + node.Level);
-//        System.out.println("-----");
-        System.out.println();
-        System.out.println("----------------------------------");
-        _printBst(node.LeftChild);
-        _printBst(node.RightChild);
+        _printBst(node.LeftChild, printArr);
+        _printBst(node.RightChild, printArr);
     }
 }
